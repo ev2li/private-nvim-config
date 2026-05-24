29d5a2788e65968a8e166353e1ff59819500ab25
ev2li <ev2li9748@163.com>
2026-05-24 11:06:05 +0800

    new config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,16 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- jk 映射为 ESC，退出插入模式（无延迟、最流畅）
+vim.keymap.set("i", "jk", "<ESC>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "i" }, "<C-z>", "<Cmd>undo<CR>", { silent = true })
+-- 空格 w 保存文件
+vim.keymap.set("n", "<space>w", "<cmd>w<CR>", { noremap = true, silent = true })
+-- 强制保存退出
+vim.keymap.set("n", "<space>r", "<cmd>source ~/.config/nvim/init.lua<CR>", { noremap = true, silent = true })
+-- save file
+vim.keymap.set({ "i", "v", "n" }, "<C-s>", "<cmd>w<cr><esc>", { noremap = true, silent = true })
+vim.keymap.set("n", "<space>fs", "<cmd>w<cr><esc>", { noremap = true, silent = true })
+vim.keymap.set("n", "qq", "<cmd>q!<cr><esc>", { noremap = true, silent = true, desc = "quit" })
+vim.keymap.set("n", "wq", "<cmd>wq<cr><esc>", { noremap = true, silent = true, desc = "save and quit" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,27 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Disable terminal bell (e.g. when moving to a line with diagnostics)
+vim.opt.errorbells = false
+vim.opt.visualbell = false
+vim.opt.belloff = "all"
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 0
+vim.opt.autoread = true
+
+-- LazyVim auto format
+vim.g.autoformat = true
+-- 透明背景，继承 Ghostty
+vim.cmd([[
+  augroup TransparentBG
+    autocmd!
+    autocmd ColorScheme * highlight Normal ctermbg=NONE guibg=NONE
+    autocmd ColorScheme * highlight NonText ctermbg=NONE guibg=NONE
+    autocmd ColorScheme * highlight SignColumn ctermbg=NONE guibg=NONE
+  augroup END
+]])
+-- 确保 256色
+vim.opt.termguicolors = true
